b7f744f623c358903397d66725caee24e0541f93
Sergey Smirnov <nomad1899@gmail.com>
2019-04-02 13:59:06 +0300

    7

--- HW3.py
+++ HW3.py
@@ -11,41 +11,47 @@ print(d)
 # (или 0, 3, 4, 5 - если индексация начинается с нуля), т.к. именно в этих позициях первого массива стоят четные числа.
 
 m = [randint(1, 100) for i in range(10)]
-n=[x for x in range(10) if m[x]%2==0]
+n = [x for x in range(10) if m[x] % 2 == 0]
 print(m)
 print(n)
 
 # 3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 from random import randint
+
 m = [randint(1, 100) for i in range(10)]
 print(m)
-m[m.index(max(m))],m[m.index(min(m))]=m[m.index(min(m))],m[m.index(max(m))]
+m[m.index(max(m))], m[m.index(min(m))] = m[m.index(min(m))], m[m.index(max(m))]
 print(m)
 
-
 # 4. Определить, какое число в массиве встречается чаще всего.
-m=[randint(1,5) for i in range(10)]
-num_count=[m.count(m[x])for x in range(10)]
-max_num_count=m[num_count.index(max(num_count))]
+m = [randint(1, 5) for i in range(10)]
+num_count = [m.count(m[x]) for x in range(10)]
+max_num_count = m[num_count.index(max(num_count))]
 print(f'Чащ всего встречаеться {max_num_count} {max(num_count)} раз.')
 
 # 5. В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию (индекс) в массиве.
 
 # 6. В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
 # Сами минимальный и максимальный элементы в сумму не включать.
-m=[randint(1,100) for i in range(10)]
-max_ind=m.index(max(m))
-min_ind=m.index(min(m))
+m = [randint(1, 100) for i in range(10)]
+max_ind = m.index(max(m))
+min_ind = m.index(min(m))
 print(m)
-if max_ind>min_ind:
-    print(sum(m[min_ind+1:max_ind]))
+if max_ind > min_ind:
+    print(sum(m[min_ind + 1:max_ind]))
 else:
-    print(sum(m[max_ind+1:min_ind]))
-
+    print(sum(m[max_ind + 1:min_ind]))
 
 # 7. В одномерном массиве целых чисел определить два наименьших элемента. Они могут быть как равны между собой
 # (оба являться минимальными), так и различаться.
+m = [randint(1, 100) for i in range(10)]
+print(min(m))
+m.remove(min(m))
+print(min(m))
+
+# 8. Матрица 5x4 заполняется вводом с клавиатуры кроме последних элементов строк.
+# Программа должна вычислять сумму введенных элементов каждой строки и записывать ее в последнюю ячейку строки.
+# В конце следует вывести полученную матрицу.
 
 
-# 8. Матрица 5x4 заполняется вводом с клавиатуры кроме последних элементов строк. Программа должна вычислять сумму введенных элементов каждой строки и записывать ее в последнюю ячейку строки. В конце следует вывести полученную матрицу.
 # 9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
